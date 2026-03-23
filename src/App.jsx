b12658fd11c69c99ea8f68c9492a4cb7e48c7065
Gabitronix1 <gabitronix1@outlook.com>
2026-03-23 16:56:44 -0300

    Change Supabase client configuration
    Updated Supabase client URL and key.

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import PanelEjecutivo from '@/components/PanelEjecutivo';
 
 
 export const supabase = createClient(
-  "https://kvenozirujsvjrsmpqhu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2ZW5vemlydWpzdmpyc21wcWh1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTM1NDM0MiwiZXhwIjoyMDYwOTMwMzQyfQ.PHyDwgHefWFTQkNKPRZ-Xdj7v6cg6j9oZ3VWTbseKLc"
+  "https://fcjembkiuilkvxqvftmo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamVtYmtpdWlsa3Z4cXZmdG1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDI4MjI4NiwiZXhwIjoyMDg5ODU4Mjg2fQ.GTXcLSE8kA7Md5gsUWVsFl5skaoUvs7LOc6PYeYAa1s"
 );
 
 function App() {
